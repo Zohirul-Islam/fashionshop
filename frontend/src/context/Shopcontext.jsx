6682178ff61d5ab98fd722b53f137f7b168fa485
bijoy <bijoycse24@gmail.com>
2025-07-31 01:59:35 +0600

    bug fix

--- frontend/src/context/Shopcontext.jsx
+++ frontend/src/context/Shopcontext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 
 export const ShopContextProvider = ({ children }) => {
   const currency = "$";
-  const backend_url = "http://localhost:4000";
+  const backend_url = "https://vercel.com/zohirul-islams-projects/fashionshop-si52";
   const delivery_fee = 10;
   const [search, setSerch] = useState("");
   const [showSearch, setShowSerch] = useState(false);
