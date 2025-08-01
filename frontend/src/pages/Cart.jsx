6cbc161a9c142d44adcb84ff46fa0fa854997b68
bijoy <bijoycse24@gmail.com>
2025-08-01 14:23:39 +0600

    input bug fix

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -38,7 +38,7 @@ useEffect(() => {
           cartData.map((item,index)=>{
             const productData = products.find((product)=>product._id === item._id );
             return (
-              <div key={index} className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
+              <div key={index} className="py-4 border-t  border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4">
                   <div className="flex items-start gap-6">
                     <img src={productData.image[0]} className="w-16 sm:w-20" alt="" />
                     <div>
@@ -49,7 +49,9 @@ useEffect(() => {
                       </div>
                     </div>
                   </div>
-                  <input onChange={(e)=>e.target.value === '' || e.target.value === '0' ? null :updateQuantity(item._id,item.size,Number(e.target.value))} className="border max-10 sm:max-w-20 px-1 sm:px-2 py-1" type="number"  min={1} defaultValue={item.quantity} />
+                  <div className="w-full">
+                    <input onChange={(e)=>e.target.value === '' || e.target.value === '0' ? null :updateQuantity(item._id,item.size,Number(e.target.value))} className="border w-[50px] px-1 sm:px-2 py-1 text-center" type="number"  min={1} defaultValue={item.quantity} />
+                  </div>
                   <img onClick={()=>updateQuantity(item._id,item.size,0)} className="w-4 mr-4 sm:w-5 cursor-pointer" src={assets.bin_icon} alt="" />
               </div>
             )
